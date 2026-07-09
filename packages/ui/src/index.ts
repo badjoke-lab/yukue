14f7cc2b98fea4273645f5ea5b9a9e7134bc6189
badjoke-lab <badjoke.lab@gmail.com>
2026-07-10 04:57:51 +0900

    ui: export A3 shared pattern types

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,9 +1,15 @@
 export { getSiteAccent, siteThemes } from "./themes.js";
 export type { SiteThemeKey } from "./themes.js";
 export type {
+  ChangeTimelineItem,
+  DesignationItem,
+  DocumentaryImage,
+  EvidenceItem,
   FooterGroup,
   NavItem,
   OccurrenceRow,
   OverviewItem,
+  PlaceItem,
+  RelationItem,
   SnapshotMetric,
 } from "./types.js";
