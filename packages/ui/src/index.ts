a5b079defd402abea3270d3fe14ff117f90996b9
badjoke-lab <badjoke.lab@gmail.com>
2026-07-10 04:50:31 +0900

    ui: export shared pattern data types

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,2 +1,10 @@
 export { getSiteAccent, siteThemes } from "./themes.js";
 export type { SiteThemeKey } from "./themes.js";
+export type {
+  FooterGroup,
+  NavItem,
+  ObservationRow,
+  OccurrenceRow,
+  OverviewItem,
+  SnapshotMetric,
+} from "./types.js";
