3d25182c991469367eb96b536710458983695836
badjoke-lab <badjoke.lab@gmail.com>
2026-07-10 09:11:56 +0900

    core: export Public Projection API

---
+++ packages/observation-core/src/index.ts
@@ -0,0 +1,12 @@
+export { buildPublicProjection } from "./public-projection.js";
+export type {
+  PublicCurrentState,
+  PublicEntityDetailProjection,
+  PublicEvidenceView,
+  PublicHtmlProjection,
+  PublicJsonProjection,
+  PublicProjectedEntity,
+  PublicProjection,
+  PublicRelationView,
+} from "./types.js";
+export { ProjectionBuildError } from "./types.js";
