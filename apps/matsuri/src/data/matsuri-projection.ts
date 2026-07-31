c0328def529bd81fdeff3d590492411324015346
badjoke-lab <badjoke.lab@gmail.com>
2026-07-31 16:21:52 +0900

    Project Matsuri maintenance bundles 30 and 31

--- apps/matsuri/src/data/matsuri-projection.ts
+++ apps/matsuri/src/data/matsuri-projection.ts
@@ -47,6 +47,8 @@ import maintenance26 from "../../../../data/public/matsuri/f2/maintenance-26.jso
 import maintenance27 from "../../../../data/public/matsuri/f2/maintenance-27.json";
 import maintenance28 from "../../../../data/public/matsuri/f2/maintenance-28.json";
 import maintenance29 from "../../../../data/public/matsuri/f2/maintenance-29.json";
+import maintenance30 from "../../../../data/public/matsuri/f2/maintenance-30.json";
+import maintenance31 from "../../../../data/public/matsuri/f2/maintenance-31.json";
 import corrections01 from "../../../../data/public/matsuri/f2/corrections-01.json";
 import corrections02 from "../../../../data/public/matsuri/f2/corrections-02.json";
 import corrections03 from "../../../../data/public/matsuri/f2/corrections-03.json";
@@ -105,6 +107,8 @@ const additiveBundles = [
   maintenance27,
   maintenance28,
   maintenance29,
+  maintenance30,
+  maintenance31,
 ];
 
 const correctionBundles = [
